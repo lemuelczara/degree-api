1d485587fbd883f86a0b1d53e9883ab595f62b22
Lemuel Coelho Zara <lemuel.czara@mercadolivre.com>
2022-01-04 12:23:56 -0300

    first commit

---
+++ src/main/java/com/meli/degreeapi/DegreeApiApplication.java
@@ -0,0 +1,13 @@
+package com.meli.degreeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DegreeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DegreeApiApplication.class, args);
+	}
+
+}
